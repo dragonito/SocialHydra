a7861213c65c3f80f452390013da6c918c15d1b2
Robin Willig <robin@dragonito.net>
2016-12-10 01:02:57 +0100

    Added bundle for twitter, facebook and vk wall posts

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use WallPosterBundle\Post\Post;
 
 class DefaultController extends Controller
 {
@@ -16,6 +17,23 @@ class DefaultController extends Controller
         #$twitter = $this->get('endroid.twitter');
         #$test = $twitter->query('statuses/update', 'POST', 'json', array('status' => "Hello World"));
         #dump($test);
+
+        $post = new Post();
+
+        $post->setMessage("Hallo Welt 2");
+
+        $provider = $this->get('wall_poster.facebook');
+
+        try
+        {
+            $post = $provider->publish($post);
+
+            dump($post);
+        }
+        catch(Exception $ex)
+        {
+            //Handle errors
+        }
 
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
