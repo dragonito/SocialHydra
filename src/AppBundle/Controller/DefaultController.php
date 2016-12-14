3babcc08dd81823880f3f50b29e7f2565b5991de
Robin Willig <robin@dragonito.net>
2016-12-14 22:29:48 +0100

    Added theme, removed testcode from controller

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use WallPosterBundle\Post\Post;
@@ -11,33 +12,10 @@ class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+     * @Template()
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
-        #$twitter = $this->get('endroid.twitter');
-        #$test = $twitter->query('statuses/update', 'POST', 'json', array('status' => "Hello World"));
-        #dump($test);
-
-        $post = new Post();
-
-        $post->setMessage("Hallo Welt 2");
-
-        $provider = $this->get('wall_poster.facebook');
-
-        try
-        {
-            $post = $provider->publish($post);
-
-            dump($post);
-        }
-        catch(Exception $ex)
-        {
-            //Handle errors
-        }
-
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir') . '/..') . DIRECTORY_SEPARATOR,
-        ]);
+	    return [];
     }
 }
