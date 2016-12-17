ec36fc70715d94462f9ea8a916a291b39ea2cf66
Robin Willig <robin@dragonito.net>
2016-12-17 15:53:28 +0100

    Added command skeleton

---
+++ src/AppBundle/Command/CreateUserCommand.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AppBundle\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class CreateUserCommand extends Command
+{
+    protected function configure()
+    {
+        // @todo: http://symfony.com/doc/current/console.html
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+
+    }
+}
